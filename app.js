467dc72849c6a4169f234c03d12b614b34a5fffb
healym3 <79485702+healym3@users.noreply.github.com>
2021-10-18 11:22:13 -0500

    Integrate MongoDb - default list and add items

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js");
 
 const app = express();
@@ -11,28 +12,73 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const workItems = [];
+
+const itemsSchema = {
+  name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item ({
+  name: "Welcome to your todolist!"
+});
+
+const item2 = new Item ({
+  name: "<-- Hit this to delete an item."
+});
+
+const item3 = new Item({
+  name: "Hit the + button to add a new item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+// Item.insertMany(defaultItems, function(err){
+//   if (err) {
+//     console.log("Something went wrong.");
+//   } else {
+//     console.log("Default items successfully added.");
+//   }
+// });
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
+  Item.find({}, function (err, foundItems){
+    if (err){
+      console.log("Unable to find items.");
+    } else {
+      if (foundItems.length === 0) {
+        Item.insertMany(defaultItems, function(err){
+          if (err) {
+            console.log("Something went wrong.");
+          } else {
+            console.log("Default items successfully added.");
+          }
+        });
+        res.redirect("/");
+      } else {
+        res.render("list", {listTitle: "Today", newListItems: foundItems});
+      }
+      //console.log(foundItems);
 
-  res.render("list", {listTitle: day, newListItems: items});
+    }
+  });
+
+
 
 });
 
 app.post("/", function(req, res){
+  const item = new Item({
+    name: req.body.newItem
+  });
+  item.save();
+  res.redirect("/");
 
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
 });
 
 app.get("/work", function(req,res){
